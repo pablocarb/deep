3c6b542b0b2480233afc502e696c5f97b6fbcee8
Pablo Carbonell <mibsspc2@pablos-mbp.home>
2017-08-20 21:18:39 +0100

    First multi-classifer, working on extending it to larger datasets

--- rnnseq.py
+++ rnnseq.py
@@ -3,6 +3,7 @@
 import tools
 import numpy as np
 from keras.preprocessing.text import hashing_trick
+from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Activation, Dropout
 from hashlib import md5
@@ -10,7 +11,7 @@ from hashlib import md5
 """ Maximum sequence length (padded with zeros) """
 MAX_SEQ_LENGTH = 1000
 """ K-mer depth """
-SEQDEPTH = 2
+SEQDEPTH = 4
 """ Hashed dictionary size """
 TOKEN_SIZE = 20
 
@@ -31,35 +32,41 @@ X = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH) )
 Xs = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE) )
 Y = np.transpose( np.array( [np.append( np.ones(n1), np.zeros(n2) ),
                              np.append( np.zeros(n1), np.ones(n2) )] ) )
-Y = Y[:,0]
- 
+# Y = Y[:,0]
+
+# Use to_categorical to convert to one-hot each entry in the sequence
+# hot1 = np_utils.to_categorical( input1 - 1 )
+
 n = 0
 for i in range(0, len(seqs)):
     for j in range(0, len(seqs[i])):
         for k in range(1, SEQDEPTH+1):
             kmers = tools.kmers( str(seqs[i][j].seq), k )
             sequence = str(' '.join( kmers ))
-            input1 = hashing_trick(sequence, hash_function= 'md5', n=TOKEN_SIZE+1)
+            input1 = np.array(hashing_trick(sequence, hash_function= 'md5', n=TOKEN_SIZE+1))
             for l in range(0, len(input1)):
                 """ Architecture 1: usehashed kmer """
                 X[n, l, k-1] = input1[l]
-                """ Architecture 2: activate input corresponding to hashed kmer  """
+                """ Architecture 2: activate input corresponding to hashed kmer (one-hot) """
                 Xs[n, l, (k-1)*TOKEN_SIZE + input1[l]-1] = 1
         n += 1
 
+""" Flip sequences (zero-padding at the start) """
 Xsr = np.flip( Xs, 1 )
 
 model = Sequential()
 model.add( LSTM(128, input_shape=(MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE),
                 dropout=0.2, recurrent_dropout=0.2) ) # , return_sequences=True) )
-# model.add( LSTM(2) )
+# model.add( LSTM(32, dropout=0.1, recurrent_dropout=0.1) )
 # model.add( Dense(100, activation='tanh') )
-model.add( Dense(1, activation='sigmoid') )
+model.add( Dense(32, activation='elu') )
+model.add( Dense(Y.shape[1], activation='sigmoid') )
 
 # Typically for LSTM, we use RMSprop(lr=0.01) optimizer
 
-model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+# model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-model.optimizer.lr = 0.01
+model.optimizer.lr = 0.001
 
 model.fit(Xsr, Y, epochs=10, batch_size=100, validation_split=0.1)
--- tools.py
+++ tools.py
@@ -20,3 +20,50 @@ def train_test_split(n, r=0.10, random=True):
     train = ix[0:split]
     test = ix[split:n]
     return train, test
+
+def dbfasta(fasfile):
+    seqdict = {}
+    for seq in SeqIO.parse(fasfile, "fasta"):
+        seqid = seq.id
+        info = seqid.split('|')
+        if len(info) >= 2:
+            uniprot = info[1]
+        seqdict[uniprot] = seq
+    return seqdict
+
+def seqinfo(infofile):
+    seqinfo = {}
+    ecinfo = {}
+    for line in open(infofile):
+        m = line.rstrip().split('\t')
+        if m[1] == 'uniprot':
+            rid = m[0]
+            uniprot = m[2]
+            rinfo = m[3].split('|')
+            try:
+                ec = m[4].split('|')
+                if uniprot not in seqinfo:
+                    seqinfo[uniprot] = []
+                seqinfo[uniprot].append( (rid, rinfo, ec) )
+                for e in ec:
+                    if e not in ecinfo:
+                        ecinfo[e] = set()
+                    ecinfo[e].add(uniprot)
+            except:
+                continue
+    return seqinfo, ecinfo
+
+def ecdataset(seqdict, ecinfo):
+    data = {}
+    for ec in ecinfo:
+        if ec not in data:
+            data[ec] = []
+        for seqid in ecinfo[ec]:
+            if seqid in seqdict:
+                seq = str(seqdict[seqid].seq)
+                data[ec].append( (seqid, seq) )
+    return data
+
+def printecinfo(data):
+    for ec in sorted(data):
+        print (ec, len(data[ec]))
