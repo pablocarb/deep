5d56522a37e7ea745858d1f4133e9caa599f8c52
mibsspc2 <pablo.carbonell@manchester.ac.uk>
2017-08-22 17:16:01 +0100

    Woking fine with hashed sequences. Testing for big sets

--- rnnseq.py
+++ rnnseq.py
@@ -13,10 +13,48 @@ from hashlib import md5
 from importlib import reload
 reload(tools)
 
+def tensorSeq(seqs, MAX_SEQ_LENGTH, SEQDEPTH, TOKEN_SIZE):
+    Xs2 = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE) )
+    """ Architecture 3: I just realised that there is no need for hashing,
+    but concatenate one-hot encoding down to desired depth """
+    for i in range(0, len(seqs)):
+        for j in range(0, len(seqs[i])):
+            aaix = tools.aaindex( seqs[i][j]  )
+            for l in range(0, len(aaix)):
+                for k in range(0, SEQDEPTH):
+                    try:
+                        Xs2[i, l, aaix[l+k] + TOKEN_SIZE*k] = 1
+                    except:
+                        continue
+    """ Flip sequences (zero-padding at the start) """
+    Xsr = np.flip( Xs2, 1 )
+    return Xsr
+
+
+def tensorSeqHashed(seqs, MAX_SEQ_LENGTH, SEQDEPTH, TOKEN_SIZE, HASH_FUNCTION= 'md5'):
+    TRAIN_BATCH_SIZE = len(seqs)
+    X = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH) )
+    Xs = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE) )
+    for i in range(0, len(seqs)):
+        for k in range(1, SEQDEPTH+1):
+            kmers = tools.kmers( seqs[i], k )
+            sequence = str(' '.join( kmers ))
+            input1 = np.array(hashing_trick(sequence, hash_function= HASH_FUNCTION, n=TOKEN_SIZE+1))
+            for l in range(0, len(input1)):
+                try:
+                    """ Architecture 1: use hashed kmer """
+                    X[i, l, k-1] = input1[l]
+                    """ Architecture 2: activate input corresponding to hashed kmer (one-hot) """
+                    Xs[i, l, (k-1)*TOKEN_SIZE + input1[l]-1] = 1
+                except:
+                    continue
+    Xsr = np.flip( Xs, 1 )
+    return Xsr
+
 """ Maximum sequence length (padded with zeros) """
 MAX_SEQ_LENGTH = 1000
 """ K-mer depth """
-SEQDEPTH = 10
+SEQDEPTH = 4
 """ Hashed dictionary size """
 TOKEN_SIZE = 20
 
@@ -28,86 +66,44 @@ seqdict = tools.dbfasta(fasfile)
 seqinfo, ecinfo = tools.seqinfo(infofile)
 data = tools.ecdataset(seqdict, ecinfo)
 
-pattern = '6.'
+pattern = ''
 eclist = set()
 for ec in ecinfo:
     if ec.startswith(pattern):
         eclist.add(ec)
 
-ectest = set(['1.4.1.13','2.2.1.2','3.1.1.3','4.1.1.11','5.1.1.1','6.1.1.18'])
-eclist = ectest
+TEST = False
+if TEST:
+    ectest = set(['1.4.1.13','2.2.1.2','3.1.1.3','4.1.1.11','5.1.1.1','6.1.1.18'])
+    eclist = ectest
 
 seqs, seqids, Y, Yids = tools.dataset(data, eclist)
 
+ix = [i for i in np.arange(0, len(seqs))]
+np.random.shuffle( ix )
+seqs = [seqs[i] for i in ix]
+seqids = [seqids[i] for i in ix]
+Y = [Y[i] for i in ix]
 
-Y =  np_utils.to_categorical(Y)
 TRAIN_BATCH_SIZE = len(seqs)
+Y =  np_utils.to_categorical(Y)
 
 print("Training set [%s]: %d; Classes: %d" % (pattern, len(seqs), len(set(Yids))))
 
-EXAMPLE = False
-if EXAMPLE:
-    set1 = 'data/1.4.1.21.fasta'
-    set2 = 'data/2.6.1.1.fasta'
-    fas1 = tools.readfasta(set1)
-    fas2 = tools.readfasta(set2)
-
-    n1 = len(fas1)
-    n2 = len(fas2)
-
-    TRAIN_BATCH_SIZE = n1 + n2
-
-    seqs = [[fas1[s] for s in sorted(fas1)], [fas2[s] for s in sorted(fas2)]]
-    Y = np.transpose( np.array( [np.append( np.ones(n1), np.zeros(n2) ),
-                                 np.append( np.zeros(n1), np.ones(n2) )] ) )
-    # Y = Y[:,0]
-
-    # Use to_categorical to convert to one-hot each entry in the sequence
-    # hot1 = np_utils.to_categorical( input1 - 1 )
-
 print("Allocating tensors...")
-X = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH) )
-Xs = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE) )
-Xs2 = np.zeros( (TRAIN_BATCH_SIZE, MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE) )
-
-""" Architecture 3: I just realised that there is no need for hashing,
-    but concatenate one-hot encoding down to desired depth """
-n = 0
-for i in range(0, len(seqs)):
-    for j in range(0, len(seqs[i])):
-        aaix = tools.aaindex( seqs[i][j]  )
-        for l in range(0, len(aaix)):
-            for k in range(0, SEQDEPTH):
-                try:
-                    Xs2[n, l, aaix[l+k] + TOKEN_SIZE*k] = 1
-                except:
-                    continue
-        n += 1
-""" Flip sequences (zero-padding at the start) """
-Xsr = np.flip( Xs2, 1 )
-
 
 HASHED = True
 if HASHED:
-    n = 0
-    for i in range(0, len(seqs)):
-        for j in range(0, len(seqs[i])):
-            for k in range(1, SEQDEPTH+1):
-                kmers = tools.kmers( str(seqs[i][j].seq), k )
-                sequence = str(' '.join( kmers ))
-                input1 = np.array(hashing_trick(sequence, hash_function= 'md5', n=TOKEN_SIZE+1))
-                for l in range(0, len(input1)):
-                    """ Architecture 1: use hashed kmer """
-                    X[n, l, k-1] = input1[l]
-                    """ Architecture 2: activate input corresponding to hashed kmer (one-hot) """
-                    Xs[n, l, (k-1)*TOKEN_SIZE + input1[l]-1] = 1
-            n += 1
-    Xsr = np.flip( Xs, 1 )
+    Xsr =  tensorSeqHashed(seqs, MAX_SEQ_LENGTH, SEQDEPTH, TOKEN_SIZE, HASH_FUNCTION= 'md5')
+else:
+    Xsr = tensorSeq(seqs, MAX_SEQ_LENGTH, SEQDEPTH, TOKEN_SIZE)
+
+
 
 print("Model definition...")
 
 model = Sequential()
-model.add( LSTM(32, input_shape=(MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE),
+model.add( LSTM(128, input_shape=(MAX_SEQ_LENGTH, SEQDEPTH*TOKEN_SIZE),
                 activation='tanh', recurrent_activation='sigmoid',
                 dropout=0.2, recurrent_dropout=0.01) ) #, return_sequences=True) )
 #model.add( LSTM(32, dropout=0.1, recurrent_dropout=0.1) )
